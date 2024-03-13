bf939a4c7e423b8458abeb635e6c3d5c95223a6e
Bigbrainium <regmi.616969@swl.k12.oh.us>
2024-03-12 21:44:04 -0400

    wowaoidfja

---
+++ src/Game.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+
+public class Game {
+
+    public static void play(GameWheel g) {
+        int totalAmount = 0;
+        ArrayList<String> list = new ArrayList<>();
+
+        for (int i = 1; i <= 3; i++) {
+            String tempColor;
+            int tempAmount;
+            Slice tempSlice = g.spinWheel();
+            tempAmount = tempSlice.getPrizeAmount();
+            tempColor = tempSlice.getColor();
+            totalAmount = totalAmount + tempAmount;
+            list.add(tempColor);
+            System.out.println("Spin " + i + " " + tempSlice.toString());
+        }
+        int counter = 0;
+        String placeholder = "";
+        for (String s : list) {
+            counter++;
+            placeholder = s;
+        }
+
+        if (counter > 1) {
+            System.out.println("You won $" + totalAmount);
+        }else{
+            System.out.println(totalAmount * 2);
+            System.out.println("All spots landed on were the same color " + placeholder + " doubling your money!");
+        }
+
+    }
+}
